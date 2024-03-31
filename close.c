2517af828837173417cdef81423466351220548c
helloworld9948 <134037451+helloworld9948@users.noreply.github.com>
2024-03-31 12:24:27 +0530

    Add files via upload

---
+++ close.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <fcntl.h>
+#include <unistd.h> // Added for close function
+
+int main() {
+    int fd1 = open("foo.txt", O_RDONLY);
+    
+    if (fd1 < 0) {
+        perror("c1");
+        exit(1);
+    }
+    
+    printf("opened the fd = %d\n", fd1);
+    
+    // Using close system Call
+    if (close(fd1) < 0) {
+        perror("c1");
+        exit(1);
+    } else {
+        printf("closed the fd.\n");
+    }
+    
+    return 0;
+}
